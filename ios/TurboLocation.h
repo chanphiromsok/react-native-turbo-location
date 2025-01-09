72f7ec616939b4470985f4f7ff6e0235296374db
Chanphirom Sok <65760336+chanphiromsok@users.noreply.github.com>
2025-01-09 13:47:48 +0700

    feat: add example of sendEvent

--- ios/TurboLocation.h
+++ ios/TurboLocation.h
@@ -1,12 +1,12 @@
+#import <React/RCTEventEmitter.h>
 
 #ifdef RCT_NEW_ARCH_ENABLED
 
-    #import "RNTurboLocationSpec.h"
-    @interface TurboLocation : NSObject <NativeTurboLocationSpec>
+#import "RNTurboLocationSpec.h"
+@interface TurboLocation : NSObject <NativeTurboLocationSpec>
 #else
-    #import <React/RCTBridgeModule.h>
-    @interface TurboLocation : NSObject <RCTBridgeModule>
+#import <React/RCTBridgeModule.h>
+@interface TurboLocation : RCTEventEmitter <RCTBridgeModule>
 
 #endif
 @end
-
